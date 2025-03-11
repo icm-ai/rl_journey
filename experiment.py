f2aeb25e1b643f09b4998a6cf07a5a6cb4d392de
MingChen <25345174+icm-ai@users.noreply.github.com>
2025-03-11 14:07:13 +0800

    refactor experiment notebook and add standalone experiment script for CartPole

---
+++ experiment.py
@@ -0,0 +1,19 @@
+"""
+Author: MingChen
+Date: 2025-03-11 14:04:42
+LastEditors: MingChen
+LastEditTime: 2025-03-11 14:04:57
+"""
+
+import gym  # 导入 Gym 的 Python 接口环境包
+import numpy as np
+import random
+import time
+
+
+env = gym.make("CartPole-v0", render_mode="rgb_array")  # 构建实验环境
+env.reset() # 重置一个回合
+for _ in range(10):
+    action = env.action_space.sample() # 从动作空间中随机选取一个动作
+    observation, reward, done, info = env.step(action) # 用于提交动作，括号内是具体的动作
+env.close() # 关闭环境
