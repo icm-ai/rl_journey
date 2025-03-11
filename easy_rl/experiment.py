c6e87d1455cdc5c5e92ce2825ac0a36a22fa7fde
MingChen <25345174+icm-ai@users.noreply.github.com>
2025-03-11 14:23:37 +0800

    enhance experiment script to render environment and output observations

--- easy_rl/experiment.py
+++ easy_rl/experiment.py
@@ -2,18 +2,18 @@
 Author: MingChen
 Date: 2025-03-11 14:04:42
 LastEditors: MingChen
-LastEditTime: 2025-03-11 14:04:57
+LastEditTime: 2025-03-11 14:22:04
 """
 
 import gym  # 导入 Gym 的 Python 接口环境包
-import numpy as np
-import random
-import time
+import numpy as np  # 导入 numpy 包
 
 
 env = gym.make("CartPole-v0", render_mode="rgb_array")  # 构建实验环境
-env.reset() # 重置一个回合
+env.reset()  # 重置一个回合
 for _ in range(10):
-    action = env.action_space.sample() # 从动作空间中随机选取一个动作
-    observation, reward, done, info = env.step(action) # 用于提交动作，括号内是具体的动作
-env.close() # 关闭环境
+    env.render()
+    action = env.action_space.sample()  # 从动作空间中随机选取一个动作
+    observation, reward, done, info = env.step(action)  # 用于提交动作，括号内是具体的动作
+    print(observation) # 输出当前观测值
+env.close()  # 关闭环境
