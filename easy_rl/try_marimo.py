52432b4fdc684bcfa5ea43e819047a682c474057
MingChen <25345174+icm-ai@users.noreply.github.com>
2025-03-28 16:10:57 +0800

    refactor try_marimo.py to enhance plotting functionality and streamline cell definitions

--- easy_rl/try_marimo.py
+++ easy_rl/try_marimo.py
@@ -7,24 +7,37 @@ app = marimo.App()
 @app.cell
 def _():
     import marimo as mo
-    return (mo,)
+    import numpy as np
+    import matplotlib.pyplot as plt
+    return mo, np, plt
+
+
+@app.cell
+def _(np, plt):
+    plt.scatter(np.linspace(0, 100, 11), np.linspace(0, 100, 11))
+    plt.show()
+    return
+
+
+@app.cell
+def _(np):
+    print(np.linspace(0, 90, 10))
+    return
 
 
 @app.cell
 def _():
-    import gym  # 导入 Gym 的 Python 接口环境包
+    return
 
-    env = gym.make("CartPole-v0", render_mode="rgb_array")  # 构建实验环境
-    env.reset()  # 重置一个回合
-    for _ in range(10):
-        env.render()
-        action = env.action_space.sample()  # 从动作空间中随机选取一个动作
-        observation, reward, done, info = env.step(
-            action
-        )  # 用于提交动作，括号内是具体的动作
-        print(observation)  # 输出当前观测值
-    env.close()  # 关闭环境
-    return action, done, env, gym, info, observation, reward
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
 
 
 @app.cell
