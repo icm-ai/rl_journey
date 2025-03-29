483d29f2e9c790500f49df05d5281a59b839c5bc
MingChen <25345174+icm-ai@users.noreply.github.com>
2025-03-29 15:23:37 +0800

    add print_evens functions to try_marimo.py for enhanced even number printing

--- easy_rl/try_marimo.py
+++ easy_rl/try_marimo.py
@@ -27,17 +27,31 @@ def _(np):
 
 @app.cell
 def _():
-    return
+    def print_evens(x: list[int]) -> None:
+        for i in (j for j in x if j % 2 == 0):
+            print(i)
+    print_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    return (print_evens,)
 
 
 @app.cell
 def _():
-    return
+    def print_evens_1(x: list[int]) -> None:
+        evens = (j for j in x if j % 2 == 0)
+        for i in evens:
+            print(i)
+
+    print_evens_1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    return (print_evens_1,)
 
 
 @app.cell
 def _():
-    return
+    def print_evens_2(x: list[int]) -> None:
+        print(*[i for i in x if i % 2 == 0], sep="\n")
+
+    print_evens_2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    return (print_evens_2,)
 
 
 @app.cell
